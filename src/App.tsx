1786a83c123e41249173b396d9b9417779255ed6
RomanSamohwal <rsamohwal@gmail.com>
2020-10-25 01:52:20 +0300

    checked api register

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import './App.css';
+import {cardsAPI} from './api/cards-api';
+import React from 'react';
 
 function App() {
-    return (
-        <div className="App">
+    const HandleSignUp = async () => {
+        try {
+            await cardsAPI.signUp('rmohwal@gmail.com', 'reactdeveloper888')
+        } catch (e) {
+            console.log(e.response.data.error)
+        }
+    }
 
+
+    return (<div className="App">
+         <button onClick={HandleSignUp}>Add</button>
         </div>
     );
 }
 
 export default App;
+
+
+/*
+data: "{"email":"rmohwal@gmail.com","password":"reactdeveloper888"}*/
--- src/api/cards-api.ts
+++ src/api/cards-api.ts
@@ -7,39 +7,54 @@ const settings = {
 }
 
 const instance = axios.create({
-    baseURL: 'localhost',
+    baseURL: localhost,
     ...settings
 })
 
 // api
 export const cardsAPI = {
-    login(email: string, password: string,) {
-        const promise = instance.post<any>('POST/auth/login', {email: email, password: password, rememberMe: true});
-        return promise;
+    login: async (email: string, password: string,) => {
+        const response = await instance.post<any>('auth/login', {email: email, password: password, rememberMe: true});
+        return response.data;
     },
-    Register(email: string, password: string) {
-        const promise = instance.post<any>('POST/auth/register', {email: email, password: password});
-        return promise;
+    signUp: async (email: string, password: string) => {
+        const response = await instance.post('auth/register', {email: email, password: password});
+        return response.data;
     },
     authMe() {
-        const promise = instance.post<any>('POST/auth/register', {});
+        const promise = instance.post<any>('auth/register', {});
         return promise;
     },
     logout() {
-        const promise = instance.delete<any>('DELETE/auth/me', {});
+        const promise = instance.delete<any>('auth/me', {});
         return promise;
     },
     forgot() {
-        const promise = instance.post<any>('POST/auth/forgot', {});
+        const promise = instance.post<any>('auth/forgot', {});
         return promise;
     },
     newPassword(password: string, resetPasswordToken: string) {
-        const promise = instance.post<any>('POST/auth/set-new-password', {password: password, resetPasswordToken});
+        const promise = instance.post<any>('auth/set-new-password', {password: password, resetPasswordToken});
         return promise;
     },
     updateUsers(name: string, avatar: string) {
-        const promise = instance.put<any>('PUT/auth/me', {name: name, avatar});
+        const promise = instance.put<any>('auth/me', {name: name, avatar});
         return promise;
-    },
+    }
 }
 
+
+
+//res login
+/*
+addedUser: {_id: "5f949e1fd7d66426389a00a1", email: "rsamo@gmail.com", rememberMe: false, isAdmin: false,…}
+created: "2020-10-24T21:35:27.241Z"
+email: "rsamo@gmail.com"
+isAdmin: false
+name: "rsamo@gmail.com"
+publicCardPacksCount: 0
+rememberMe: false
+updated: "2020-10-24T21:35:27.241Z"
+verified: false
+__v: 0
+_id: "5f949e1fd7d66426389a00a1"*/
