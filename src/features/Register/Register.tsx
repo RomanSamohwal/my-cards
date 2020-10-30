ae880f5f52aa730d65232be33cf40f3e9b216d9f
RomanSamohwal <rsamohwal@gmail.com>
2020-10-30 22:36:19 +0300

    add formik register. Add validation

--- src/features/Register/Register.tsx
+++ src/features/Register/Register.tsx
@@ -1,5 +1,98 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {FormikHelpers, useFormik} from 'formik';
+
+type FormValuesType = {
+    email: string,
+    password: string,
+    password_confirm: string
+}
 
 export const Register = () => {
-    return <></>
+    const [disable, setDisable] = useState<boolean>(true)
+
+    const formik = useFormik({
+        validate: (values) => {
+            if (!values.email) {
+                setDisable(true)
+                return {
+                    email: 'Email is required'
+                }
+            }
+            if (!values.password) {
+                setDisable(true)
+                return {
+                    password: 'Password is required'
+                }
+            }
+            if (!values.password_confirm) {
+                setDisable(true)
+                return {
+                    password_confirm: 'confirm your password'
+                }
+            }
+            if (values.password !== values.password_confirm) {
+                setDisable(true)
+                return {
+                    password_confirm: 'The entered password is incorrect'
+                }
+            } else {
+                setDisable(false)
+            }
+        },
+        initialValues: {
+            email: '',
+            password: '',
+            password_confirm: ''
+        },
+        onSubmit: (values, formikHelpers: FormikHelpers<FormValuesType>) => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
+
+    return <>
+        <form onSubmit={formik.handleSubmit}>
+            <label htmlFor="email">Enter your mail and password</label>
+            <div style={{
+                height: '150px',
+                display: 'flex', flexDirection: 'column',
+                justifyContent: 'space-around', alignItems: 'center'
+            }}>
+                <div>
+                    <input
+                        id="email"
+                        name="email"
+                        type="email"
+                        onChange={formik.handleChange}
+                        value={formik.values.email}
+                    />
+                    {formik.errors.email ? <div>{formik.errors.email}</div> : null}
+                </div>
+                <div>
+                    <input
+                        id='password'
+                        name='password'
+                        type='password'
+                        onChange={formik.handleChange}
+                        value={formik.values.password}
+                    />
+                    {formik.errors.password ? <div>{formik.errors.password}</div> : null}
+                </div>
+                <div>
+                    <input
+                        id='password_confirm'
+                        name='password_confirm'
+                        type='password'
+                        onChange={formik.handleChange}
+                        value={formik.values.password_confirm}
+                    />
+                    {formik.errors.password_confirm ? <div>{formik.errors.password_confirm}</div> : null}
+                </div>
+                <div>
+                    <button type="submit" disabled={disable}>Add</button>
+                </div>
+            </div>
+        </form>
+    </>
 }
+
+
